d6e15603304eb0450cca80fbe13618681e5370fd
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-09-22 12:17:54 -0400

    Better operation displays

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from matrix import Matrix
 
-mat = Matrix()
+mat = Matrix([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
 mat.display()
 
 mat.swap(1, 2)
@@ -10,4 +14,8 @@ mat.sum(0, 1)
 mat.display()
 
 mat.multiply(10, 2)
+mat.display()
+
+print(mat.get_entry(1, 2))
+mat.set_entry(1, 2, 3)
 mat.display()
--- matrix.py
+++ matrix.py
@@ -1,35 +1,33 @@
 class Matrix:
-    matrix = []
-
-    def __init__(self):
-        self.matrix = [[1, 2, 3],
-                       [4, 5, 6],
-                       [7, 8, 9]]
+    def __init__(self, matrix):
+        self.matrix = matrix
 
     def get_entry(self, row: int, col: int) -> int:
-        return self.matrix[row][col]
+        print(f">>> GET({row}, {col}) <<<")
+        return self.matrix[row-1][col-1]
 
     def set_entry(self, row: int, col: int, value: int) -> None:
-        self.matrix[row][col] = value
+        print(f">>> SET({row}, {col}, {value}) <<<")
+        self.matrix[row-1][col-1] = value
 
     def swap(self, row_a: int, row_b: int) -> None:
-        print(f"Swap row {row_a} and row {row_b}...")
+        print(f">>> SWAP({row_a}, {row_b}) <<<")
 
-        temp = self.matrix[row_a]
-        self.matrix[row_a] = self.matrix[row_b]
-        self.matrix[row_b] = temp
+        temp = self.matrix[row_a-1]
+        self.matrix[row_a-1] = self.matrix[row_b-1]
+        self.matrix[row_b-1] = temp
 
     def multiply(self, scalar: float, row: int) -> None:
-        print(f"Multiply row {row} by scalar {scalar}...")
+        print(f">>> MULTIPLY({scalar}, {row}) <<<")
 
-        for i in range(len(self.matrix[row])):
-            self.matrix[row][i] *= scalar
+        for i in range(len(self.matrix[row-1])):
+            self.matrix[row-1][i] *= scalar
 
     def sum(self, from_row: int, to_row: int) -> None:
-        print(f"Adding row {from_row} to row {to_row}...")
+        print(f">>> ADD({from_row}, {to_row}) <<<")
 
-        for i, entry in enumerate(self.matrix[from_row]):
-            self.matrix[to_row][i] += entry
+        for i, entry in enumerate(self.matrix[from_row-1]):
+            self.matrix[to_row-1][i] += entry
 
     def display(self) -> None:
         for row in self.matrix:
