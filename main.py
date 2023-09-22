667e0e97a735e308c96d42e3b606e4153003e6b3
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-09-22 12:31:18 -0400

    Begin rref implementation

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 from matrix import Matrix
 
 mat = Matrix([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [1, 1, 1, -1],  # x = -9
+    [1, 2, 4, 3],   # y = 10
+    [1, 3, 9, 3]    # z = -2
 ])
 mat.display()
 
-mat.swap(1, 2)
-mat.display()
-
-mat.sum(0, 1)
-mat.display()
-
-mat.multiply(10, 2)
-mat.display()
-
-print(mat.get_entry(1, 2))
-mat.set_entry(1, 2, 3)
-mat.display()
+mat.rref()
--- matrix.py
+++ matrix.py
@@ -1,12 +1,19 @@
+from typing import List
+
+
 class Matrix:
     def __init__(self, matrix):
         self.matrix = matrix
 
-    def get_entry(self, row: int, col: int) -> int:
+    def get(self, row: int, col: int) -> int:
         print(f">>> GET({row}, {col}) <<<")
         return self.matrix[row-1][col-1]
 
-    def set_entry(self, row: int, col: int, value: int) -> None:
+    def get_row(self, row: int) -> List[int]:
+        print(f">>> GET_ROW({row}) <<<")
+        return self.matrix[row-1]
+
+    def set(self, row: int, col: int, value: int) -> None:
         print(f">>> SET({row}, {col}, {value}) <<<")
         self.matrix[row-1][col-1] = value
 
@@ -23,15 +30,25 @@ class Matrix:
         for i in range(len(self.matrix[row-1])):
             self.matrix[row-1][i] *= scalar
 
-    def sum(self, from_row: int, to_row: int) -> None:
+    def add(self, from_row: int, to_row: int) -> None:
         print(f">>> ADD({from_row}, {to_row}) <<<")
 
         for i, entry in enumerate(self.matrix[from_row-1]):
             self.matrix[to_row-1][i] += entry
 
+    # find alternative ways to tabulate
     def display(self) -> None:
         for row in self.matrix:
             for entry in row:
                 print(entry, end=" ")
             print()
         print("-----")
+
+    # assuming 3x3 system, generalize later
+    def rref(self: List[List[int]]) -> None:
+        # First row pivot to 1
+        self.multiply(1/self.get(1, 1), 1)
+
+        # Eliminate x
+        self.add(1, 2)
+        self.add(1, 3)
