2d67892eeb3d784aa27fe11cd35a1250148eb719
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-10-13 12:08:58 -0400

    Improve on printing

--- 2023-10-13/matrix.py
+++ 2023-10-13/matrix.py
@@ -1,4 +1,5 @@
 from typing import List
+from tabulate import tabulate
 
 
 class Matrix:
@@ -6,43 +7,44 @@ class Matrix:
         self.matrix = matrix
 
     def get(self, row: int, col: int) -> int:
-        print(f">>> GET({row}, {col}) <<<")
+        # print(f">>> GET({row}, {col}) <<<")
         return self.matrix[row-1][col-1]
 
     def get_row(self, row: int) -> List[int]:
-        print(f">>> GET_ROW({row}) <<<")
+        # print(f">>> GET_ROW({row}) <<<")
         return self.matrix[row-1]
 
     def set(self, row: int, col: int, value: int) -> None:
-        print(f">>> SET({row}, {col}, {value}) <<<")
+        # print(f">>> SET({row}, {col}, {value}) <<<")
         self.matrix[row-1][col-1] = value
 
     def swap(self, row_a: int, row_b: int) -> None:
-        print(f">>> SWAP({row_a}, {row_b}) <<<")
+        print(f">>> SWAP(row_a: {row_a}, row_b: {row_b}) <<<")
 
         temp = self.matrix[row_a-1]
         self.matrix[row_a-1] = self.matrix[row_b-1]
         self.matrix[row_b-1] = temp
 
+        self.display()
+
     def multiply(self, scalar: float, row: int) -> None:
-        print(f">>> MULTIPLY({scalar}, {row}) <<<")
+        print(f">>> MULTIPLY(scalar: {scalar}, row: {row}) <<<")
 
         for i in range(len(self.matrix[row-1])):
             self.matrix[row-1][i] *= scalar
 
+        self.display()
+
     def add(self, from_row: int, to_row: int) -> None:
-        print(f">>> ADD({from_row}, {to_row}) <<<")
+        print(f">>> ADD(from_row: {from_row}, to_row: {to_row}) <<<")
 
         for i, entry in enumerate(self.matrix[from_row-1]):
             self.matrix[to_row-1][i] += entry
 
-    # find alternative ways to tabulate
+        self.display()
+
     def display(self) -> None:
-        for row in self.matrix:
-            for entry in row:
-                print(entry, end=" ")
-            print()
-        print("-----")
+        print(tabulate(self.matrix) + "\n")
 
     # assuming 3x3 system, generalize later
     def rref(self: List[List[int]]) -> None:
