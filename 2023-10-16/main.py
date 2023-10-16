ed8214077b25bdb986bfddc7f3d3606b2dca309f
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-10-16 12:17:13 -0400

    Finish graph -> matrix

--- 2023-10-16/main.py
+++ 2023-10-16/main.py
@@ -14,9 +14,20 @@ def matrix_to_graph(matrix: List[List[int]]) -> nx.Graph:
 
 def graph_to_matrix(graph: nx.Graph) -> List[List[int]]:
     matrix = []
+    cols = len(graph.nodes())
 
     for edge in graph.edges():
-        print(edge)  # wip
+        row = []
+
+        for i in range(cols):
+            if i == edge[0]:
+                row.append(1)
+            elif i == edge[1]:
+                row.append(-1)
+            else:
+                row.append(0)
+
+        matrix.append(row)
 
     return matrix
 
