3d2a38e9d90d1d82b3a612e48d68c5d79e0a018b
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-10-16 11:53:26 -0400

    Start implementing matrix <--> graph

---
+++ 2023-10-16/README.md
@@ -0,0 +1,13 @@
+# Graphs in Python and NetworkX
+
+Anthony Chen, Linear Algebra, 10/16/23
+
+## Features
+
+- Builds a graph from an incidence matrix
+- Builds an incidence matrix from a graph
+
+## WIP
+
+- Finds loops
+- Produces a sequence of graphs where edges are eliminated by finding loops
---
+++ 2023-10-16/main.py
@@ -0,0 +1,44 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from typing import List
+
+
+def matrix_to_graph(matrix: List[List[int]]) -> nx.Graph:
+    graph = nx.Graph()
+
+    for row in matrix:
+        graph.add_edge(row.index(1), row.index(-1))
+
+    return graph
+
+
+def graph_to_matrix(graph: nx.Graph) -> List[List[int]]:
+    matrix = []
+
+    for edge in graph.edges():
+        print(edge)  # wip
+
+    return matrix
+
+
+def main() -> None:
+    matrix = [
+        [1, -1, 0, 0],
+        [1, 0, -1, 0],
+        [1, 0, 0, -1],
+        [0, 1, -1, 0],
+        [0, 1, 0, -1],
+        [0, 0, 1, -1]
+    ]
+
+    graph = matrix_to_graph(matrix)
+
+    # nx.draw(graph, with_labels=True, node_color="lightblue")
+    # plt.show()
+
+    matrix_2 = graph_to_matrix(graph)
+    print(matrix_2)
+
+
+if __name__ == "__main__":
+    main()
